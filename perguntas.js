8118e527fdc28d5e6d0cafe66449f01cac243f8e
kauapassareli <165683475+kauapassareli@users.noreply.github.com>
2024-10-28 12:26:12 -0300

    GXGD

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criaCartao(
+'Progamação',
+'O Python é uma linguagem de programação'
+)
+criaCartao(
+'Geografia',
+'Quala capitalda França',
+'A capital da frança é Paris'
+)
+criaCartao(
+ 'Programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa algua terefa'
+) 
+criaCartao( 
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hi(RAI)'
+)
